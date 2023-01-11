5979f9c08e684f81caa4725e26234eca4a4288e9
David Olanipekun <olaide.olanipekun@yahoo.com>
2023-01-11 20:55:02 +0000

    create user api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,7 +5,7 @@ from django.contrib.auth import (
     get_user_model,
     # authenticate,
 )
-from django.utils.translation import gettext as _
+# from django.utils.translation import gettext as _
 
 from rest_framework import serializers
 
