1869ce9b5af4524738ed7d38a8f4d2add47a4c7a
David Olanipekun <olaide.olanipekun@yahoo.com>
2023-01-21 00:16:15 +0000

    feat:tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -31,6 +31,16 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id']
 
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed."""
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
@@ -41,20 +51,27 @@ class RecipeSerializer(serializers.ModelSerializer):
             tag_obj, creeated = Tag.objects.get_or_create(
                 user=auth_user, **tag)
             recipe.tags.add(tag_obj)
-        # self._get_or_create_tags(tags, recipe)
+        self._get_or_create_tags(tags, recipe)
         # self._get_or_create_ingredients(ingredients, recipe)
 
         return recipe
 
-    # def _get_or_create_tags(self, tags, recipe):
-    #     """Handle getting or creating tags as needed."""
-    #     auth_user = self.context['request'].user
-    #     for tag in tags:
-    #         tag_obj, created = Tag.objects.get_or_create(
-    #             user=auth_user,
-    #             **tag,
-    #         )
-    #         recipe.tags.add(tag_obj)
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
